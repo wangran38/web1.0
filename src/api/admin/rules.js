88740d18f854b75fc1252d05358d984928e2bbc9
wangran38 <wangrantian@qq.com>
2022-07-26 18:05:13 +0800

    增加了菜单的页面

---
+++ src/api/admin/rules.js
@@ -0,0 +1,33 @@
+import request from '@/utils/request'
+
+export function getlist(params) {
+  return request({
+    url: '/admin/getruleslist',
+    method: 'post',
+    data: params
+  })
+}
+
+export function addRules(params) {
+  return request({
+    url: '/admin/addrules',
+    method: 'post',
+    data: params
+  })
+}
+
+export function editMenu(params) {
+  return request({
+    url: '/admin/editrules',
+    method: 'post',
+    data: params
+  })
+}
+
+export function deleteMenu(params) {
+  return request({
+    url: '/admin/delrules',
+    method: 'post',
+    data: params
+  })
+}
